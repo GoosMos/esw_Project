da6792c1fd9dc62559e696b259381368c54d54ae
Goosmos <bdt040321@gmail.com>
2023-11-19 19:01:24 +0900

    ..

--- JungDeaMan.py
+++ JungDeaMan.py
@@ -7,16 +7,22 @@ from PIL import Image, ImageDraw, ImageFont
 from adafruit_rgb_display import st7789
 import numpy as np
 
+# 좌상단 (0, 0), 우하단 (240, 240)
+
 class JungDeaMan:
-    def __init__(self) :
+    def __init__(self, width, height) :
         # 정대만이 가져야하는 변수들
         # 팔각도, 외형, 시작점
 
         # 팔각도는 0 ~ 90도
         self.shoulderAngel = 45
         self.power = 0
-        # 힘은 
-        self.position = np.array([0, 0, 0, 0])
+
+        self.appearance = 'circle'
+        self.outline = "#FFFFFF"
+
+
+        self.position = np.array([0, 220 , 40, 240])
 
 
     def move(self, command = None) :
@@ -25,10 +31,24 @@ class JungDeaMan:
         if command['up_pressed']:
             if (self.shoulderAngel != 90) : self.shoulderAngel += 5
             print("정대만의 팔 각도 상승!!")
-            print("현재 팔각도 : " + self.shoulderAngel)
+            print("현재 팔각도 : ", self.shoulderAngel)
     
         
         if command['down_pressed']:
             if (self.shoulderAngel != 0) : self.shoulderAngel -= 5
             print("정대만의 팔 각도 하강!!")
-            print("현재 팔각도 : " + self.shoulderAngel)
+            print("현재 팔각도 : ", self.shoulderAngel)
+
+        if command['left_pressed']:
+            if (self.power != 0) : 
+                self.power -= 1
+                self.position[0] -= 5
+                self.position[2] -= 5
+            print("정대만의 파워 감소")
+        
+        if command['right_pressed']:
+            if (self.power != 10) : 
+                self.power += 1
+                self.position[0] += 5
+                self.position[2] += 5
+            print("정대만의 파워 증가")
--- ver1.py
+++ ver1.py
@@ -9,14 +9,16 @@ import numpy as np
 import JungDeaMan
 import Joystick
 
-JungDaeMan = JungDeaMan.JungDeaMan()
 joystick = Joystick.Joystick()
+JungDaeMan = JungDeaMan.JungDeaMan(joystick.width, joystick.height)
+
+# 이미지의 모드 설정 == 디스플레이 전체에 대한 초기화
 my_image = Image.new("RGB", (joystick.width, joystick.height)) 
 my_draw = ImageDraw.Draw(my_image)
 my_draw.rectangle((0, 0, joystick.width, joystick.height), fill = (255, 255, 255, 100))
 print("HElloo")
 
-my_draw.rectangle((0, 0, joystick.width, joystick.height), fill = (255, 255, 255, 100))
+
 
 while True:
     command = {'move': False, 'up_pressed': False , 'down_pressed': False, 'left_pressed': False, 'right_pressed': False}
@@ -37,8 +39,13 @@ while True:
         command['right_pressed'] = True
         command['move'] = True
 
+    #if not joystick.button_A.value:
+
+
+
     JungDaeMan.move(command)
 
     #그리는 순서가 중요합니다. 배경을 먼저 깔고 위에 그림을 그리고 싶었는데 그림을 그려놓고 배경으로 덮는 결과로 될 수 있습니다.
     my_draw.rectangle((0, 0, joystick.width, joystick.height), fill = (255, 255, 255, 100))
+    my_draw.ellipse(tuple(JungDaeMan.position), outline = JungDaeMan.outline, fill = (0, 0, 0))
     joystick.disp.image(my_image)
